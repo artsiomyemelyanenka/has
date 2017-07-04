457610872d6ec67b8620a45220035cffeaf8dec0
Artsiom Yemelyanenka <Artsiom_Yemelyanenka@epam.com>
2017-07-04 18:05:38 +0300

    corrext restoreSimple

--- src/main/java/com/epam/hybris/has/client/commands/LoadSettingsFromProperties.java
+++ src/main/java/com/epam/hybris/has/client/commands/LoadSettingsFromProperties.java
@@ -1,10 +1,14 @@
 package com.epam.hybris.has.client.commands;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Iterator;
 
 import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.PropertiesConfiguration;
+
+import com.epam.hybris.has.client.Main;
 
 
 /**
@@ -18,25 +22,40 @@ public class LoadSettingsFromProperties implements Cmd
 	public void execute(CmdContext context)
 	{
 		File propertiesFile = new File(context.getBaseFolder(), HAS_PROPERTIES);
-		if (! propertiesFile.exists()) {
-			context.addErrorMessage("Can't find file " + HAS_PROPERTIES + " in the working folder.");
-			return;
-		}
 		PropertiesConfiguration props = new PropertiesConfiguration();
-		try
+		if (!propertiesFile.exists())
 		{
-			props.load(propertiesFile);
-			Iterator keys = props.getKeys();
-			while(keys.hasNext()) {
-				String key = keys.next().toString();
-				context.set(key, props.getString(key));
+			context.message("Can't find file " + HAS_PROPERTIES + " in the working folder. Will load it from internal resources.");
+			try (InputStream re = Main.class.getClassLoader().getResourceAsStream("has.properties"))
+			{
+				props.load(re);
+			}
+			catch (Exception e)
+			{
+				e.printStackTrace();
+				context.addErrorMessage(e);
+				return;
 			}
 		}
-		catch (ConfigurationException e)
+		else
 		{
-			e.printStackTrace();
+			try
+			{
+				props.load(propertiesFile);
+			}
+			catch (ConfigurationException e)
+			{
+				e.printStackTrace();
+				context.addErrorMessage(e);
+				return;
+			}
 		}
-
+		Iterator keys = props.getKeys();
+		while (keys.hasNext())
+		{
+			String key = keys.next().toString();
+			context.set(key, props.getString(key));
+		}
 	}
 
 }
